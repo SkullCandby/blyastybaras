aebd9c4fd5ccf007a7d6adc7df4a0f31d5f4e0ad
code_creator <42152517+SkullCandby@users.noreply.github.com>
2024-02-07 04:13:53 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <v-app>
+    <v-main style="background-color: #FCFDC7;">
+      <v-container fill-height>
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="6">
+            <LogoComponent class="mt-12"/>
+            <ComingSoonComponent class="mt-28"/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <FooterComponent/>
+  </v-app>
+</template>
+
+<script>
+import LogoComponent from "./components/LogoComponent.vue";
+import ComingSoonComponent from "./components/ComingSoonComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+export default {
+  name: 'App',
+  components: {
+    FooterComponent,
+    LogoComponent,
+    ComingSoonComponent
+  }
+}
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+body {
+  font-family: 'Press Start 2P', cursive;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <footer class="footer">
+      <v-container fluid>
+        <v-row justify="center" align="center">
+          <v-col cols="12" class="d-flex align-center justify-center">
+            <span class="follow-text">follow us:</span>
+            <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" class="twitter-icon">
+              <!-- Use an img tag for your Twitter icon -->
+              <img :src="twitterLogo" alt="Twitter" class="icon"/>
+            </a>
+          </v-col>
+        </v-row>
+      </v-container>
+    </footer>
+  </template>
+  
+  <script>
+  import twitterLogo from '@/assets/twit-logo.png'; // Make sure the path is correct
+  
+  export default {
+    name: 'FooterComponent',
+    data() {
+      return {
+        twitterLogo
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color: #FCFDC7;
+    padding: 1rem;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+  }
+  
+  .follow-text, .twitter-icon {
+    display: flex;
+    align-items: center;
+  }
+  
+  .twitter-icon {
+    margin-left: 0.5rem; /* Adjust as needed */
+  }
+  
+  .icon {
+    height: 2rem; /* Or whatever size you prefer */
+  }
+  </style>
